f81e1aff8a01636556ab1defc3b6d5704750e74a
MrLightHA <lighthaexe@gmail.com>
2025-06-17 16:38:50 +0300

    remove example in main class

--- src/main/java/me/lightha/lhc/LHC.java
+++ src/main/java/me/lightha/lhc/LHC.java
@@ -2,20 +2,8 @@ package me.lightha.lhc;
 
 import lombok.Getter;
 import me.lightha.lhc.particle.ParticleManager;
-import me.lightha.lhc.particle.impl.ParticleBuilder;
 import me.lightha.lhc.particle.impl.ParticleManagerImpl;
-import me.lightha.lhc.particle.shape.impl.CircleShape;
-import me.lightha.lhc.settings.impl.ParticleSettings;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Particle;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
 
 @Getter
 public final class LHC extends JavaPlugin {
@@ -27,15 +15,6 @@ public final class LHC extends JavaPlugin {
         // Plugin startup logic
        instance = this;
        this.particleManager = new ParticleManagerImpl();
-       Location location = new Location(Bukkit.getWorld("world"), 0,150,0);
-       List<Player> players = new ArrayList<>(Arrays.asList(Bukkit.getPlayer("LightHA")));
-       new ParticleBuilder()
-               .viewers(players)
-               .settings(new ParticleSettings().type(Particle.REDSTONE).posY(0).radius(5).speed(0).rgb("255;255;255"))
-               .location(location)
-               .shape(new CircleShape())
-               .duration(600)
-               .build(UUID.randomUUID());
     }
 
     @Override
